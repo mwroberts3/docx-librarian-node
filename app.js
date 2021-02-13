cadd931253e7f79e0416375f5d1f81aaaf40124b
Mike <mwroberts89@gmail.com>
2021-02-13 12:31:01 -0500

    one more time with the port business

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const express = require('express');
 const app = express();
-const port = 80;
+const port = process.env.PORT || 5000;
 
 const multer = require('multer');
 const bodyParser = require('body-parser');
